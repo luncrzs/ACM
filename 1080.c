f5681dae66ee7235adf9fba93e86246fae7bb730
Zhenhao Luo <luncrzs@gmail.com>
2013-05-01 22:57:55 +0900

    1080Edited

--- 1080.c
+++ 1080.c
@@ -3,35 +3,38 @@ int digit(int n);
 
 int main(void)
 {
-	int a,i=0,j;
-        int arr[100000];
-	while(scanf("%d",&a)==1)
+	int a;
+    char num[100];
+	while(gets(num))
 	{
 
-            if(a==0)
-                break;
-            else
-            {
-                do{
-                        a=digit(a);
-                }while(a>=10);
-            }
-                arr[i++]=a;
-	}
-        for(j=0;j<i;j++)
+        if(num[0]=="0")
+            return 0;
+        else
         {
-            printf("%d\n",arr[j]);
+            a=digit(num);
+            printf("%d\n",a);
         }
+	}
 	return 0;
 }
 
-int digit(int n)
+int digit(char num[100])
 {
-	int i,dig=0;
-	while(n>=1)
-	{
-		dig=dig+n%10;
-		n=n/10;
-	}
+	int i,dig=0,n=0;
+    for(i=0;num[i]!="\0";i++)
+    {
+        n=n+num[i]-48;
+    }
+    do{
+        dig=0;
+        while(n>=1)
+        {
+            dig=dig+n%10;
+            n=n/10;
+        }
+        n=dig;
+    }while(dig>=10)
+    	
 	return dig;
 }
